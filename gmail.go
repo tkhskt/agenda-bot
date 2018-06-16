2be42a3420495985462756af16badf94530ea3d2
gericass <bonjovi1926@yahoo.co.jp>
2018-06-17 01:45:44 +0900

    add: file save function

--- gmail.go
+++ gmail.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"time"
-	"log"
-
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/gmail/v1"
 	"io/ioutil"
 	"encoding/json"
+	"encoding/base64"
+	"strings"
+	"errors"
+	"os"
 )
 
 type token struct {
@@ -19,17 +21,36 @@ type token struct {
 	RefreshToken string `json:"refreshToken"`
 }
 
-func main() {
+type file struct {
+	filename string
+	dec      []byte
+}
 
+func decode(str string) ([]byte, error) {
+	if strings.ContainsAny(str, "+/") {
+		return nil, errors.New("invalid base64url encoding")
+	}
+	str = strings.Replace(str, "-", "+", -1)
+	str = strings.Replace(str, "_", "/", -1)
+	for len(str)%4 != 0 {
+		str += "="
+	}
+	return base64.StdEncoding.DecodeString(str)
+}
+
+func getToken() (*token, error) {
 	t, err := ioutil.ReadFile("./token.json")
 	if err != nil {
-		fmt.Errorf("%v", err)
+		return nil, err
 	}
-
-	var tk token
-
+	var tk *token
 	json.Unmarshal(t, &tk)
 
+	return tk, nil
+
+}
+
+func getGmailService(tk *token) (*gmail.Service, error) {
 	config := oauth2.Config{
 		ClientID:     tk.ClientId,
 		ClientSecret: tk.ClientSecret,
@@ -50,23 +71,88 @@ func main() {
 
 	srv, err := gmail.New(client)
 	if err != nil {
-		log.Fatalf("Unable to retrieve gmail Client %v", err)
+		return nil, err
+	}
+	return srv, nil
+}
+
+func (fl *file) createFile() error {
+	filePath := "./file/" + fl.filename
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := f.Write(fl.dec); err != nil {
+		return err
+	}
+	if err := f.Sync(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getFileFromMessage(part *gmail.MessagePart, srv *gmail.Service, ms *gmail.Message) ([]byte, error) {
+	umas := gmail.NewUsersMessagesAttachmentsService(srv)
+	atc := umas.Get("me", ms.Id, part.Body.AttachmentId)
+	dt, err := atc.Do()
+	if err != nil {
+		return nil, err
+	}
+	dec, err := decode(dt.Data)
+	if err != nil {
+		return nil, err
+	}
+	return dec, nil
+}
+
+func searchFileFromMessage(part []*gmail.MessagePart, ms *gmail.Message, srv *gmail.Service) (*file, error) {
+	for _, v := range part {
+		if strings.Contains(v.Filename, "アジェンダ") {
+			dec, err := getFileFromMessage(v, srv, ms)
+			if err != nil {
+				return nil, err
+			}
+			return &file{v.Filename, dec}, nil
+		}
+	}
+	return nil, nil
+}
+
+func main() {
+	token, err := getToken()
+	if err != nil {
+		fmt.Errorf("token error: %v", err)
+	}
+	srv, err := getGmailService(token)
+	if err != nil {
+		fmt.Errorf("getService error: %v", err)
 	}
 
 	ums := gmail.NewUsersMessagesService(srv)
 
 	r, err := srv.Users.Messages.List("me").Do()
 	if err != nil {
-		log.Fatalf("Unable to get labels. %v", err)
+		fmt.Errorf("get message list error: %v", err)
 	}
 
 	for _, v := range r.Messages {
 		ms, err := ums.Get("me", v.Id).Do()
 		if err != nil {
-			log.Fatalf("%v", err)
+			fmt.Errorf("%v", err)
 		}
-		fmt.Println(ms.Snippet)
-		fmt.Println(ms.Payload.Filename)
+		fl, err := searchFileFromMessage(ms.Payload.Parts, ms, srv)
+		if err != nil {
+			fmt.Errorf("search file error: %v", err)
+		}
+		if fl != nil {
+			err = fl.createFile()
+			if err != nil {
+				fmt.Errorf("create file error: %v", err)
+			}
+			// 最新アジェンダのみ作成
+			break
+		}
 	}
-
 }
